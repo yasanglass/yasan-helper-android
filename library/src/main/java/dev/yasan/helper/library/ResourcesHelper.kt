68e2481f32e4c78696028d829ca134e9ca8a0110
Yasan Ghafariyan <yasandev@gmail.com>
2021-03-01 17:17:54 +0330

    Add isColorDark & isColorLight functions

--- library/src/main/java/dev/yasan/helper/library/ResourcesHelper.kt
+++ library/src/main/java/dev/yasan/helper/library/ResourcesHelper.kt
@@ -2,6 +2,7 @@ package dev.yasan.helper.library
 
 import android.content.Context
 import androidx.core.content.ContextCompat
+import androidx.core.graphics.ColorUtils
 
 /**
  * A helper class with a set of functions that help with resources related difficulties in Android applications.
@@ -12,6 +13,7 @@ object ResourcesHelper {
     /**
      * Gets the requested Color using [ContextCompat]
      * @param resourceId the Color's resource id
+     * @author Yasan Ghafariyan
      */
     fun getColorWithContext(context: Context, resourceId: Int) =
         ContextCompat.getColor(context, resourceId)
@@ -19,9 +21,24 @@ object ResourcesHelper {
     /**
      * Gets the requested Drawable using [ContextCompat]
      * @param resourceId the Drawable's resource id
+     * @author Yasan Ghafariyan
      */
     fun getDrawableWithContext(context: Context, resourceId: Int) =
         ContextCompat.getDrawable(context, resourceId)
+
+    /**
+     * Checks if the color Int value is a dark or light.
+     * @return If the color's luminance is <0.5.
+     * @author Yasan Ghafariyan
+     */
+    fun isColorDark(color: Int) = ColorUtils.calculateLuminance(color) < 0.5
+
+    /**
+     * Checks if the color Int value is a dark or light.
+     * @return If the color's luminance is >0.5.
+     * @author Yasan Ghafariyan
+     */
+    fun isColorLight(color: Int) = !isColorDark(color)
 
 }
 
@@ -38,3 +55,17 @@ fun Context.getColorWithContext(resourceId: Int) =
  */
 fun Context.getDrawableWithContext(resourceId: Int) =
     ResourcesHelper.getDrawableWithContext(this, resourceId)
+
+/**
+ * Checks if the color Int value is a dark or light.
+ * @return If the color's luminance is <0.5.
+ * @author Yasan Ghafariyan
+ */
+fun Int.isColorDark() = ResourcesHelper.isColorDark(this)
+
+/**
+ * Checks if the color Int value is a dark or light.
+ * @return If the color's luminance is >0.5.
+ * @author Yasan Ghafariyan
+ */
+fun Int.isColorLight() = ResourcesHelper.isColorLight(this)
